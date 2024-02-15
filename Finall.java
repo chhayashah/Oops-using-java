8672ae4222c1781c5780e58749b51029db445d64
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2024-02-15 11:21:57 +0530

    Add files via upload

---
+++ Finall.java
@@ -0,0 +1,10 @@
+public class Finall {
+    final String name = "chhaya";
+    int a;
+
+    public static void main(String[] args) {
+        Finall r1 = new Finall();
+        // r1.name="kiran"; //In final keyword we can'nt re-assign value it's throw an error cannot assign a value to final variable name
+        System.out.println(r1.name);
+    }
+}
