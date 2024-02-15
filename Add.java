6c27dcdfa555d16355911395acd969230876fca7
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2024-02-15 11:20:26 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add {
+    int a;
+    int b;
+
+    public static void main(String[] args){
+        Add r1=new Add();
+        r1.a=10;
+        r1.b=20;
+        System.out.println(r1.a+r1.b);
+    }
+}
